a51463c162930232939753d1b933680a0ed46aeb
Dzianis_Shyla <Dzianis_Shyla@EPBYMINW3897>
2017-04-11 12:06:55 +0300

    simple controller added

--- ToDoClient/Scripts/Angular/toDoController.js
+++ ToDoClient/Scripts/Angular/toDoController.js
@@ -1,1 +1,8 @@
-﻿
+﻿angular.module('toDoList').controller('toDoController', ['$scope', 'toDoService',
+    function ($scope, toDoService) {
+        $scope.hello = 'hello';
+        $scope.addClick = function()
+        {
+            $scope.hello = 'hi';
+        }
+    }]);
